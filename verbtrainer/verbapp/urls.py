e0cc744c01c442ff1e7134b31156d5e364242165
AlenKir <kirillova.alena@gmail.com>
2024-06-02 17:52:18 +0200

    fixes #1

--- verbtrainer/verbapp/urls.py
+++ verbtrainer/verbapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import IrregularVerbListView, TrainerView, VerbUpdateView, AddVerbView
 
+app_name = 'verbapp'
+
 urlpatterns = [
     path('verb_list/', IrregularVerbListView.as_view(), name='verb_list'),
     path('trainer/', TrainerView.as_view(), name='trainer'),
