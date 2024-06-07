4e0f8ea6d8b56b980efbebefab8ef64b78d76708
AlenKir <kirillova.alena@gmail.com>
2024-06-07 07:52:04 +0200

    session level

--- verbtrainer/verbtrainer/views.py
+++ verbtrainer/verbtrainer/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 def index(request):
-    return render(request, 'verbtrainer/index.html')
+    if request.method == 'POST':
+        level = request.POST.get('level')
+        request.session['level'] = level
+        return redirect(reverse('index'))
+    level = request.session.get('level')
+    return render(request, 'verbtrainer/index.html',
+                  {'level': level})
