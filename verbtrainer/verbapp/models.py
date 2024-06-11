f595fa189523d218fe509ccfd68ac95b4fcb0688
AlenKir <kirillova.alena@gmail.com>
2024-06-11 11:57:30 +0200

    spaced rep fixes #3

--- verbtrainer/verbapp/models.py
+++ verbtrainer/verbapp/models.py
@@ -9,6 +9,7 @@ class IrregularVerb(models.Model):
     translation = models.CharField(max_length=100)
     past_simple = models.CharField(max_length=100, verbose_name="Präteritum")
     past_participle = models.CharField(max_length=100, verbose_name="Partizip II")
+    memory_score = models.IntegerField(default=0)
 
     class Meta:
         unique_together = ('base', 'past_simple', 'past_participle')
@@ -22,10 +23,12 @@ class IrregularVerb(models.Model):
 
     @staticmethod
     def get_random_verb_as_task(hide_forms=1):
-        verbs = list(IrregularVerb.objects.all())
-        v_id = random.choice(verbs).__dict__['id']
-        verb = IrregularVerb.objects.filter(id=v_id).first()
-        return verb.get_task(hide_forms)
+        ordered_verbs = IrregularVerb.objects.all().order_by('memory_score')
+        max_score = max(verb.memory_score for verb in ordered_verbs)
+        max_score = max_score if max_score != 0 else 1e-6
+        weights = [(max_score - verb.memory_score) / max_score for verb in ordered_verbs]
+        chosen_verb = random.choices(ordered_verbs, weights=weights)[0]
+        return chosen_verb.get_task(hide_forms)
 
     def get_task(self, hide_forms=1):
         forms = ['base', 'past_simple', 'past_participle']
@@ -41,11 +44,25 @@ class IrregularVerb(models.Model):
     def check_forms(self, answer):
         results = answer.copy()
         error_fields = []
+        memory_score = 0
+
         assert int(getattr(self, "id")) == int(answer["id"]), "IDs do not match"
         del answer["id"]
+
         for field in answer:
             solution = getattr(self, field)
             if solution != answer[field]:
                 error_fields.append(field)
                 results[field] += f" (correct: {solution})"
+
+        if len(error_fields) == 0:
+            memory_score += 1
+        else:
+            memory_score -= 1
+        self.update_memory_score(memory_score)
+
         return results, error_fields
+
+    def update_memory_score(self, change):
+        self.memory_score += change
+        self.save()
