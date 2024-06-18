e064984281fd0fa301cd4f35a6f5473ba247085f
AlenKir <kirillova.alena@gmail.com>
2024-06-18 11:57:38 +0200

    basic auth functionality, fixes #15

--- verbtrainer/verbtrainer/urls.py
+++ verbtrainer/verbtrainer/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('verbapp/', include('verbapp.urls', namespace='verbapp')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
